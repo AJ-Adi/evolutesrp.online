0f112212bb26fde4e6bc28ea0a8e691e7039e31b
AJ-Adi <114731564+AJ-Adi@users.noreply.github.com>
2023-04-22 22:32:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.getElementById('content-container').style.display = 'block';
+    document.querySelector('.loader').style.display = 'none';
+  }, 2000); 
+});
+
+function connectToFivem() {
+  window.open("fivem://connect/evolutesrp.online:30120", "_self");
+}
